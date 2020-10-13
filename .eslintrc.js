1f0917a940f8402ebe6f0981199924cc206d1022
parfumes <xxparfumes@gmail.com>
2020-10-14 00:23:55 +0900

    Update config files to correct eslint error

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,62 +1,64 @@
-export const parser = '@typescript-eslint/parser';
-export const extends = [
-  'plugin:react/recommended',
-  'plugin:@typescript-eslint/recommended',
-  'plugin:prettier/recommended',
-];
-export const parserOptions = {
-  project: ['tsconfig.json', '@typescript-eslint/parser'],
-  ecmaVersion: 2020,
-  sourceType: 'module',
-  ecmaFeatures: {
-    jsx: true,
-  },
-};
-export const env = {
-  es6: true,
-};
-export const plugins = [
-  '@typescript-eslint',
-  'react',
-  'react-hooks',
-  'eslint-plugin-import',
-  'prettier',
-];
-export const rules = {
-  'prettier/prettier': [
-    'error',
-    {
-      singleQuote: true,
-      trailingComma: 'all',
-    },
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
-  '@typescript-eslint/explicit-function-return-type': 'off',
-  '@typescript-eslint/no-unused-vars': 'off',
-  'react/jsx-filename-extension': [
-    'warn',
-    {
-      extensions: ['.jsx', '.tsx'],
-    },
-  ],
-  'react/prop-types': [
-    1,
-    {
-      ignore: ['context', 'tracking'],
-    },
-  ],
-  'react-hooks/rules-of-hooks': 'error',
-  'react-hooks/exhaustive-deps': 'warn',
-};
-export const settings = {
-  react: {
-    version: 'detect',
-  },
-};
-export const overrides = [
-  {
-    files: ['*.ts', '*.tsx'],
-    rules: {
-      'react/prop-types': 'off',
+  parserOptions: {
+    project: ['tsconfig.json'],
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
   },
-];
+  env: {
+    es6: true,
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'eslint-plugin-import',
+    'prettier',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'all',
+      },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/prop-types': [
+      1,
+      {
+        ignore: ['context', 'tracking'],
+      },
+    ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'react/prop-types': 'off',
+      },
+    },
+  ],
+};
