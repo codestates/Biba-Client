47eb89cd3e779205fb00516ec633d9f412ff605b
parfumes <xxparfumes@gmail.com>
2020-10-13 21:51:33 +0900

    Init Project 2

--- biba/.eslintrc.js
+++ biba/.eslintrc.js
@@ -1,43 +1,63 @@
 module.exports = {
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   extends: [
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
   ],
   parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "module",
+    project: ['tsconfig.json'],
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   env: {
     es6: true,
   },
-  plugins: ["prettier"],
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'react-hooks',
+    'eslint-plugin-import',
+    'prettier',
+  ],
   rules: {
-    "prettier/prettier": [
-      "error",
+    'prettier/prettier': [
+      'error',
       {
         singleQuote: true,
-        trailingComma: "all",
+        trailingComma: 'all',
       },
     ],
-    "react/prop-types": [
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.tsx'],
+      },
+    ],
+    'react/prop-types': [
       1,
       {
-        ignore: ["context", "tracking"],
+        ignore: ['context', 'tracking'],
       },
     ],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
   settings: {
     react: {
-      version: "detect",
+      version: 'detect',
     },
   },
   overrides: [
     {
-      files: ["*.ts", "*.tsx"],
+      files: ['*.ts', '*.tsx'],
       rules: {
-        "react/prop-types": "off",
+        'react/prop-types': 'off',
       },
     },
   ],
