fafd977de47bba0d8be10158034e6bf4cd395430
zi <sjm891114@gmail.com>
2020-10-14 22:56:50 +0900

    In progress
    1. add page folder
    2. update global-style.ts nomalize css

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import './App.css';
 import { theme } from './styles/theme';
 import { ThemeProvider } from 'styled-components';
 
-import { TodayBeerList } from './components/list/TodayBeerList';
+import { Home } from './components/page/Home';
 
 const App: React.FC = () => {
   return (
     <ThemeProvider theme={theme}>
       <Switch>
         <Route path='/'>
-          <TodayBeerList />
+          <Home />
         </Route>
       </Switch>
     </ThemeProvider>
