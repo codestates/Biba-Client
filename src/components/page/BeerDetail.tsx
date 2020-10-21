6a7c54346576030943f1e0a110ad3b16f79b34d8
parfumes <xxparfumes@gmail.com>
2020-10-21 16:32:46 +0900

    complete BeerDetail BM

--- src/components/page/BeerDetail.tsx
+++ src/components/page/BeerDetail.tsx
@@ -1,80 +1,146 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
 import { BeerDetailProps } from '../../containers/page/BeerDetailContainer';
 
 export const BeerDetail = ({
   match,
   beerDetail,
+  bookmark,
+  handleBookmark,
+  story,
+  more,
+  handleInfoTab,
+  handleStar,
   handleClickAllReviews,
 }: BeerDetailProps): JSX.Element => {
   const { id } = match.params;
   const example = 'https://bit.ly/2T6JRuE';
 
   return (
-    <Container>
-      <InfoArea>
-        <InfoImg src={example} alt='' />
-        <InfoDiv>
-          <InfoTitle>맥주 상세 정보</InfoTitle>
-          <FavToggle>
-            즐겨찾기<input type='checkbox'></input>
-          </FavToggle>
-          <InfoSub>
-            <H2>{beerDetail.beer_name}</H2>
-            <H4>별점 평균{beerDetail.rate}</H4>
-            <H4>오렌지, 뜨거운 맥주, 벨기에</H4>
-          </InfoSub>
-          <Info>
-            <InfoDetail1>
-              <H4>맥주 스토리</H4>
-              <P>이 맥주에는 이런 전설이 있습니다</P>
-            </InfoDetail1>
-            <InfoDetail2>
-              <H4>상세 정보</H4>
-              <P>제조사: {beerDetail.company}</P>
-              <P>제조국: {beerDetail.country}</P>
-              <P>스타일: {beerDetail.style_name}</P>
-              <P>
-                ABV / IBU: {beerDetail.abv} / {beerDetail.ibu}
-              </P>
-            </InfoDetail2>
-          </Info>
-        </InfoDiv>
-      </InfoArea>
-      <RatingArea>별점주기</RatingArea>
-      <CommentArea></CommentArea>
-      <WriteComment>리뷰 작성하기</WriteComment>
-      <CommentAll onClick={handleClickAllReviews}>리뷰 전체보기</CommentAll>
-    </Container>
+    <Outer>
+      <Container className='detailContainer'>
+        <InfoArea className='infoArea'>
+          <InfoImg className='infoImg' src={example} alt='' />
+          <InfoDiv className='infoDiv'>
+            <InfoTitle className='infoTitle'>맥주 상세 정보</InfoTitle>
+            <FavToggle className='favToggle'>
+              즐겨찾기
+              <input
+                type='checkbox'
+                onChange={handleBookmark}
+                checked={bookmark}
+              ></input>
+            </FavToggle>
+            <InfoSub className='infoSub'>
+              <BeerName className='beerName'>{beerDetail.beer_name}</BeerName>
+              <Average className='beerAverage'>
+                별점 평균 {beerDetail.rate}
+              </Average>
+              <Tag className='beerTag'>오렌지, 뜨거운 맥주, 벨기에</Tag>
+            </InfoSub>
+            <InfoBody className='infoBody'>
+              <Tab className='infoTab'>
+                <Tab1 id='story' onClick={handleInfoTab}>
+                  맥주 스토리&nbsp;&nbsp;|&nbsp;
+                </Tab1>
+                <Tab2 id='more' onClick={handleInfoTab}>
+                  &nbsp;상세 정보
+                </Tab2>
+              </Tab>
+              <InfoDetail1
+                className='infoDetail'
+                style={story ? { display: 'block' } : { display: 'none' }}
+              >
+                <P className='content'>이 맥주에는 이런 전설이 있습니다</P>
+              </InfoDetail1>
+              <InfoDetail2
+                className='infoDetail'
+                style={more ? { display: 'block' } : { display: 'none' }}
+              >
+                <P className='content'>제조사: {beerDetail.company}</P>
+                <P className='content'>제조국: {beerDetail.country}</P>
+                <P className='content'>스타일: {beerDetail.style_name}</P>
+                <P className='content'>
+                  ABV / IBU: {beerDetail.abv} / {beerDetail.ibu}
+                </P>
+              </InfoDetail2>
+            </InfoBody>
+          </InfoDiv>
+        </InfoArea>
+        <RatingArea className='ratingArea'>
+          <Rate className='rate'>별점 주기</Rate>
+          <Stars className='stars'>{handleStar()}</Stars>
+        </RatingArea>
+        <CommentArea className='commentArea'>
+          <WriteComment className='commentBtn'>리뷰 작성하기</WriteComment>
+          <CommentAll className='commentAllBtn' onClick={handleClickAllReviews}>
+            리뷰 전체보기
+          </CommentAll>
+        </CommentArea>
+      </Container>
+    </Outer>
   );
-};
+}; // 별점주기 마무리, info display
 
-const Container = styled.div``;
+const Outer = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding: 2em 0 2em 3em;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
 
 const InfoArea = styled.div`
   display: flex;
 `;
-const InfoImg = styled.img``;
+const InfoImg = styled.img`
+  width: 25em;
+`;
 const InfoDiv = styled.div``;
 
-const InfoTitle = styled.h2``;
+const InfoTitle = styled.p`
+  font-size: 1.4em;
+`;
 
 const FavToggle = styled.div``;
 
 const InfoSub = styled.div``;
-const Info = styled.div``;
+const InfoBody = styled.div``;
 
-const H2 = styled.h2``;
-const H4 = styled.h4`
+const BeerName = styled.p``;
+const Average = styled.p`
   font-size: 1.2em;
 `;
+const Tag = styled.p``;
 const P = styled.p``;
 
+const Tab = styled.div`
+  display: flex;
+  font-size: 1.2em;
+`;
+const Tab1 = styled.p`
+  cursor: pointer;
+`;
+const Tab2 = styled.p`
+  cursor: pointer;
+`;
 const InfoDetail1 = styled.div``;
 const InfoDetail2 = styled.div``;
 
-const RatingArea = styled.div``;
+const RatingArea = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const Rate = styled.p``;
+const Stars = styled.div`
+  display: flex;
+`;
 const CommentArea = styled.div``;
 
 const WriteComment = styled.button``;
--- src/containers/page/BeerDetailContainer.tsx
+++ src/containers/page/BeerDetailContainer.tsx
@@ -4,15 +4,24 @@ import { useSelector, useDispatch } from 'react-redux';
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 import axios from 'axios';
 import styled from 'styled-components';
+import { FaRegStar, FaStar, FaStarHalfAlt } from 'react-icons/fa';
 
 import { RootState } from '../../modules';
 import { ContentType } from '../../modules/nav'; // Empty, Login, MypageAllReviews
+import { starStatusInit } from '../../modules/beerdetail';
 import { BeerDetail } from '../../components/page/BeerDetail';
-import { IBeerDetail, aReview } from '../../modules/beerdetail';
+import { Bookmark, IBeerDetail, aReview } from '../../modules/beerdetail';
 import { DefaultProps } from '../../containers/page/HomeContainer';
+import { checkStarScore } from './pageUtils';
 
 export interface BeerDetailProps extends DefaultProps {
   beerDetail: IBeerDetail;
+  bookmark: boolean;
+  handleBookmark(): void;
+  story: boolean;
+  more: boolean;
+  handleInfoTab(e: React.MouseEvent<HTMLElement>): void;
+  handleStar(): JSX.Element[];
   handleClickAllReviews(): void;
 }
 
@@ -25,14 +34,90 @@ const BeerDetailContainer = ({
     (state: RootState) => state.login,
   );
   const { beerDetail } = useSelector((state: RootState) => state.beerDetail);
+  const { bookmark } = useSelector((state: RootState) => state.bookmark);
+  const { review, star, starScore } = useSelector(
+    (state: RootState) => state.userReview,
+  );
+  const { story, more } = useSelector((state: RootState) => state.infoStatus);
+  const { a, b, c, d, e } = useSelector((state: RootState) => state.starStatus);
+  const currentStatus = [a, b, c, d, e];
 
   // 이하 함수는 리스트 페이지에 작성 -> 특정 맥주 클릭 시 get 요청 -> 스토어에 정보 들어감
   const dispatch = useDispatch();
   const handleModal = (contentType: ContentType, display: boolean): void => {
     dispatch({ type: 'SET_MODAL', contentType, display });
   };
+
+  const handleBookmark = (): void => {
+    // 즐겨찾기에 check 표시 어떻게 가져올지 논의
+    axios
+      .get<Bookmark>(`http://localhost:4000/posts/info/4`) // post
+      // {
+      //   beerId: match.params.id,
+      //   userId: userData.id,
+      // })
+      .then((res) => {
+        const { bookmark } = res.data; // 추가되었을 경우 true 돌아옴
+        if (res.status === 200) {
+          dispatch({ type: 'SET_BOOKMARK', bookmark }); // store에 저장된 bookmark 상태 업데이트
+          bookmark
+            ? alert(`즐겨찾기에 추가되었습니다.`)
+            : alert(`즐겨찾기에서 삭제되었습니다.`);
+        } else {
+          alert(`즐겨찾기 추가에 실패하였습니다.`);
+        }
+      });
+  };
+  const handleInfoTab = (e: React.MouseEvent<HTMLElement>): void => {
+    console.log('test');
+    if (e.currentTarget.id === 'story') {
+      const story = true,
+        more = false;
+      dispatch({ type: 'SET_INFOSTATUS', story, more });
+    }
+    if (e.currentTarget.id === 'more') {
+      const story = false,
+        more = true;
+      dispatch({ type: 'SET_INFOSTATUS', story, more });
+    }
+  };
+  const handleClickStar = (star: React.MouseEvent<HTMLElement>): void => {
+    console.log(star.currentTarget.id);
+    if (currentStatus.indexOf(true) !== -1) {
+      dispatch({ type: 'SET_STARSTATUS', starStatusInit });
+      // axios.post - 서버에 전송, 이때 review status 체크해서 보내야 함
+      return;
+    }
+    const { a, b, c, d, e } = checkStarScore(Number(star.currentTarget.id)); // 별점 dispatch 준비 함수, boolean 객체 돌려줌
+    dispatch({ type: 'SET_STARSTATUS', a, b, c, d, e }); // 최초 진입 시 내가 준 별점 store에 저장
+    // axios.post - 서버에 전송, 이때 review status 체크해서 보내야 함
+  };
   const handleClickAllReviews = (): void => {
     handleModal(ContentType.AllReviews, true);
+  };
+
+  const handleStar = (): JSX.Element[] => {
+    const stars = [
+      [a, 1],
+      [b, 2],
+      [c, 3],
+      [d, 4],
+      [e, 5],
+    ];
+    // onclick시 ele 값 넘기는 함수
+    return stars.map((ele) => {
+      return (
+        <StarWrap
+          key={`star${ele[1]}`}
+          className='star'
+          id={`${ele[1]}`}
+          onClick={handleClickStar}
+        >
+          <FStar style={!ele[0] ? { display: 'block' } : { display: 'none' }} />
+          <EStar style={ele[0] ? { display: 'block' } : { display: 'none' }} />
+        </StarWrap>
+      );
+    });
   };
 
   return (
@@ -41,9 +126,22 @@ const BeerDetailContainer = ({
       history={history}
       location={location}
       beerDetail={beerDetail}
+      bookmark={bookmark}
+      handleBookmark={handleBookmark}
+      story={story}
+      more={more}
+      handleInfoTab={handleInfoTab}
+      handleStar={handleStar}
       handleClickAllReviews={handleClickAllReviews}
     />
   );
 };
 
 export const BeerDetailWithRouter = withRouter(BeerDetailContainer);
+
+const StarWrap = styled.div`
+  cursor: pointer;
+  display: flex;
+`;
+const FStar = styled(FaRegStar)``;
+const EStar = styled(FaStar)``;
